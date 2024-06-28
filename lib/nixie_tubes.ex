0e976f45f080f857f2f15a5a908b92ee1efba845
James Edward Gray II <james@graysoftinc.com>
2024-06-28 08:06:39 -0500

    mix new

---
+++ lib/nixie_tubes.ex
@@ -0,0 +1,18 @@
+defmodule NixieTubes do
+  @moduledoc """
+  Documentation for `NixieTubes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NixieTubes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nixie_tubes_test.exs
@@ -0,0 +1,8 @@
+defmodule NixieTubesTest do
+  use ExUnit.Case
+  doctest NixieTubes
+
+  test "greets the world" do
+    assert NixieTubes.hello() == :world
+  end
+end
